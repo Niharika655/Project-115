80069ed3045f1a34d5873325a2780fe786ca3177
Niharika655 <73527574+Niharika655@users.noreply.github.com>
2021-07-22 16:48:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide()
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized")
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("left shoulder x=" + results[0].pose.leftShoulder.x);
+        console.log("left shoulder y=" + results[0].pose.leftShoulder.y);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+}
+
+function takeSnapshot() {
+    save("Captured_Image.png");
+}
